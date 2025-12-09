d6d3592628001ad15e0424eec459033845bc1aea
Tudor.Macovei <tudor_macovei@iCloud.com>
2025-12-09 20:13:25 +0100

    Add ability to focus on an object by clicking on it
    - Rotation now only available while object is in focus
    
    - Additionally, cleaned up code to bring it in line with basic Godot conventions

--- object_manipulation/object_rotator.gd
+++ object_manipulation/object_rotator.gd
@@ -1,17 +1,23 @@
 extends Node3D
 
+# position of the object when it is in focus
+@export var focus_position: Node3D
+
+# outline of object on mouse hover
+@export var outline_material : Material
+
 enum RotatorState {
-	OnTable, # Resting on the table, cannot be interacted with except by clicking to begin the interaction
-	Stationary,
-	RotatingLeft,
-	RotatingRight,
-	RotatingUp,
-	RotatingBottom
+	ON_TABLE, # Resting on the table, cannot be interacted with except by clicking to begin the interaction
+	STATIONARY,
+	ROTATING_LEFT,
+	ROTATING_RIGHT,
+	ROTATING_UP,
+	ROTATING_BOTTOM,
 }
 
-var rotatorState = RotatorState.OnTable
-var rotationRemaining = 0.0
-var elapsedTime = 0.0
+var _rotator_state = RotatorState.ON_TABLE
+var _rotation_remaining = 0.0
+var _is_mouse_on_object = false
 
 static var ANIMATION_TIME = 0.1
 
@@ -21,28 +27,38 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	handle_rotation(delta)
-	#print("test")
+	_handle_rotation(delta)
 
-func _input(event: InputEvent) -> void:
-	# disregard input if rotation ongoing
-	if rotatorState != RotatorState.Stationary:
+func _input(event: InputEvent) -> void:	
+	if event.is_action_released("mouse_click_left"):
+		print("LOG: Mouse Click")
+		if _rotator_state == RotatorState.ON_TABLE && _is_mouse_on_object:
+			_rotator_state = RotatorState.STATIONARY
+			# TODO: remove outline on focus gain
+			$Object.global_position = focus_position.global_position
+		if _rotator_state == RotatorState.STATIONARY && !_is_mouse_on_object:
+			_rotator_state = RotatorState.ON_TABLE
+			# TODO: add outline on focus loss
+			$Object.position = Vector3.ZERO
+
+	# Object can only rotate from stationary beginning
+	if _rotator_state != RotatorState.STATIONARY:
 		return
 	
 	# would be cool to be able to use a match block here, but looks like
-	# it wouldn't really work
+	# it wouldn't really work because of the function call :/
 	if event.is_action_pressed("object_rotate_bottom"):
-		rotatorState = RotatorState.RotatingBottom
-		rotationRemaining = 1.0
+		_rotator_state = RotatorState.ROTATING_BOTTOM
+		_rotation_remaining = 1.0
 	if event.is_action_pressed("object_rotate_top"):
-		rotatorState = RotatorState.RotatingUp
-		rotationRemaining = 1.0
+		_rotator_state = RotatorState.ROTATING_UP
+		_rotation_remaining = 1.0
 	if event.is_action_pressed("object_rotate_left"):
-		rotatorState = RotatorState.RotatingLeft
-		rotationRemaining = 1.0
+		_rotator_state = RotatorState.ROTATING_LEFT
+		_rotation_remaining = 1.0
 	if event.is_action_pressed("object_rotate_right"):
-		rotatorState = RotatorState.RotatingRight
-		rotationRemaining = 1.0
+		_rotator_state = RotatorState.ROTATING_RIGHT
+		_rotation_remaining = 1.0
 
 # takes value of a value x between 0.0 and 1.0 and applies a nonlinear
 # transformation that keeps the endpoints at 0.0 and 1.0, respectively
@@ -54,45 +70,45 @@ func ease_function(x : float) -> float:
 # to an approach that *sets* the object rotation every tick instead of calling
 # the rotate(...) function. That way it will be much easier to set custom
 # rotation curves to handle the animation 
-func handle_rotation(delta: float) -> void:
-	if rotatorState == RotatorState.OnTable:
+func _handle_rotation(delta: float) -> void:
+	if _rotator_state == RotatorState.ON_TABLE:
 		return
 	
 	# to_rotate is from 0.0 to 1.0 here
-	var to_rotate = delta / ANIMATION_TIME * ease_function(rotationRemaining) 
+	var to_rotate = delta / ANIMATION_TIME * ease_function(_rotation_remaining) 
 	
-	if to_rotate > rotationRemaining:
-		to_rotate = rotationRemaining
-		rotationRemaining = 0.0
+	if to_rotate > _rotation_remaining:
+		to_rotate = _rotation_remaining
+		_rotation_remaining = 0.0
 	else:
-		rotationRemaining -= to_rotate
+		_rotation_remaining -= to_rotate
 	
 	# to_rotate is converted to values from 0.0 to PI / 2 here
 	to_rotate *= PI / 2
 	
-	match rotatorState:
-		RotatorState.RotatingBottom:
+	match _rotator_state:
+		RotatorState.ROTATING_BOTTOM:
 			$Object.rotate(Vector3.RIGHT, to_rotate)
 			pass
-		RotatorState.RotatingUp:
+		RotatorState.ROTATING_UP:
 			$Object.rotate(Vector3.RIGHT, -to_rotate)
 			pass
-		RotatorState.RotatingLeft:
+		RotatorState.ROTATING_LEFT:
 			$Object.rotate(Vector3.UP, -to_rotate)
 			pass
-		RotatorState.RotatingRight:
+		RotatorState.ROTATING_RIGHT:
 			$Object.rotate(Vector3.UP, to_rotate)
 			pass
 
-	if rotationRemaining == 0.0:
-		rotatorState = RotatorState.Stationary
+	if _rotation_remaining <= 0.0:
+		_rotator_state = RotatorState.STATIONARY
 
-
-@export var outline_material : Material
 var _original_mesh: Mesh = null
 
 # Add outline to mesh and lift it slightly
 func _on_object_mouse_entered() -> void:
+	_is_mouse_on_object = true
+
 	var mesh_instance := $Object.get_child(0) as MeshInstance3D
 	if mesh_instance == null:
 		return
@@ -125,10 +141,11 @@ func _on_object_mouse_entered() -> void:
 		mesh_clone.surface_set_material(i, new_mat)
 		
 	$Object.position.z += 0.1
-	rotatorState = RotatorState.Stationary
 
 # Restore original mesh without outline material and original position
 func _on_object_mouse_exited() -> void:
+	_is_mouse_on_object = false
+
 	# Restore original mesh
 	var mesh_instance := $Object.get_child(0) as MeshInstance3D
 	if mesh_instance == null: # 'as' keyword casts to null on type mismatch
@@ -142,4 +159,3 @@ func _on_object_mouse_exited() -> void:
 	_original_mesh = null
 	
 	$Object.position.z -= 0.1
-	rotatorState = RotatorState.OnTable
