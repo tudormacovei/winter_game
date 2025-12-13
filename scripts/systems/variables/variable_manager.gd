5cf4278fa8f9aaf083e763e2f92cc7eb5730d1c8
Ziana Tudose <tudosesanziana@gmail.com>
2025-12-13 18:33:01 +0100

    Dialogue: variable manager error checking and rename test_dialogue to showcase

--- scripts/systems/variables/variable_manager.gd
+++ scripts/systems/variables/variable_manager.gd
@@ -9,15 +9,29 @@ func _ready():
 	if globals:
 		variables = globals.get_initial_state()
 	else:
-		push_error("Global variables could not be initialized!")
+		_debug_error("Global variables could not be initialized!")
 
 
 func get_var(var_name: String):
+	if not variables.has(var_name):
+		_debug_error("Variable '%s' does not exist!" % var_name)
+		return
+		
 	return variables.get(var_name)
 
 func set_var(var_name: String, value):
 	if not variables.has(var_name):
-		push_error("Variable '%s' does not exist!" % var_name)
+		_debug_error("Variable '%s' does not exist!" % var_name)
 		return
-
+	if typeof(variables[var_name]) != typeof(value):
+		_debug_error("Variable type mis-match for var '%s' and value '%s'!" % [var_name, value])
+		return
+	
 	variables[var_name] = value
+
+
+static func _debug_error(message: String):
+	push_error(message)
+	if OS.has_feature("debug"):
+		OS.alert(message)
+	
