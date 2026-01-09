a5eb1fee036caae673c78bc36f30750538d34960
Ziana Tudose <tudosesanziana@gmail.com>
2026-01-09 23:29:18 +0100

    Add definitions for character-specific global variables

---
+++ scripts/systems/variables/globals_characters_definition.gd
@@ -0,0 +1,15 @@
+class_name GlobalsCharacterDefinition
+extends Resource
+
+const VariableDefinition = preload("res://scripts/systems/variables/variable_definition.gd")
+
+## Must exactly match the character ID of the corresponding CharacterDefinition resource!
+@export var character_id: String = ""
+@export var definitions: Array[VariableDefinition] = []
+
+func get_initial_state() -> Dictionary:
+	var globals_char := {} # Key: character_id + "_" + name, Value: default_value
+	for def in definitions:
+		globals_char[character_id + "_" + def.name] = def.default_value
+	
+	return globals_char
--- scripts/systems/variables/globals_definition.gd
+++ scripts/systems/variables/globals_definition.gd
@@ -1,20 +1,42 @@
+## Defines global variables, both general and for each character
 @tool
 class_name GlobalVariablesDefinition
 extends Resource
 
 const VariableDefinition = preload("res://scripts/systems/variables/variable_definition.gd")
+const GlobalsCharacterDefinition = preload("res://scripts/systems/variables/globals_characters_definition.gd")
 
+## General global variable definitions
 @export var definitions: Array[VariableDefinition] = []
+## Character-specific global variable definitions
+@export var character_definitions: Array[GlobalsCharacterDefinition] = []
 
-@export var dump_vars_path: String = "res://data/global_variables_overview.txt"
+#region Tool Variables
+
+const DUMP_VARS_PATH: String = "res://data/global_variables_overview.txt"
 
 @export_tool_button("Dump Globals To File")
 var dump_button := dump_to_file
 
+#endregion Tool Variables
+
 func get_initial_state() -> Dictionary:
 	var globals := {}
+	
+	# Add general globals
 	for def in definitions:
+		if globals.has(def.name):
+			Utils.debug_error("Duplicate global variable name detected: '%s'. Overwriting previous value." % def.name)
 		globals[def.name] = def.default_value
+	
+	# Add character-specific globals
+	for char_def in character_definitions:
+		var char_globals := char_def.get_initial_state()
+		for key in char_globals.keys():
+			if globals.has(key):
+				Utils.debug_error("Duplicate character global variable name detected: '%s'. Overwriting previous value." % key)
+			globals[key] = char_globals[key]
+	
 	return globals
 
 #region Tool Functions
@@ -25,9 +47,9 @@ func dump_to_file() -> void:
 		return
 
 	#NOTE: Opening in WRITE mode clears existing contents of file / creates new file
-	var file := FileAccess.open(dump_vars_path, FileAccess.WRITE)
+	var file := FileAccess.open(DUMP_VARS_PATH, FileAccess.WRITE)
 	if file == null:
-		push_error("Failed to open file: " + dump_vars_path)
+		push_error("Failed to open file: " + DUMP_VARS_PATH)
 		return
 
 	file.store_line("Generated at: %s" % Utils.get_timestamp_string())
@@ -39,12 +61,26 @@ func dump_to_file() -> void:
 			continue
 
 		file.store_line("[%d] %s" % [i, def.name])
-		file.store_line("  Description: %s" % def.description)
+		if def.description != "":
+			file.store_line("  Description: %s" % def.description)
 		file.store_line("  Default: %s" % str(def.default_value))
 		file.store_line("")
 
+	for char_def in character_definitions:
+		file.store_line("Character ID: %s" % char_def.character_id)
+		for j in char_def.definitions.size():
+			var def := char_def.definitions[j]
+			if def == null:
+				continue
+
+			file.store_line("  [%d] %s" % [j, def.name])
+			if def.description != "":
+				file.store_line("    Description: %s" % def.description)
+			file.store_line("    Default: %s" % str(def.default_value))
+			file.store_line("")
+
 	file.close()
 
-	print("Global variables dumped to file at path: ", dump_vars_path)
+	print("Global variables dumped to file at path: ", DUMP_VARS_PATH)
 
 #endregion Tool Functions
