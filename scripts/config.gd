9bead811239255852cba8cb33cb7295f257608ba
Ziana Tudose <tudosesanziana@gmail.com>
2026-02-16 23:01:02 +0100

    Dialogue: Add indirect sabotage variables

--- scripts/config.gd
+++ scripts/config.gd
@@ -4,6 +4,10 @@ extends Node
 const DAY_RESOURCES_PATH: String = "res://data/days/"
 const CHARACTER_RESOURCES_PATH: String = "res://data/characters/"
 
+const SCORE_SPECIAL_OBJECT_VAR_KEY_PREFIX: String = "prog_score_special_object_"
+const SCORE_SIMPLE_OBJECTS_VAR_KEY: String = "prog_score_simple_objects" # NOTE: This needs to be synced with the variable name in global_variables.tres
+const SCORE_SIMPLE_OBJECTS_SMOOTHING_FACTOR: float = 0.2 # Higher value = score will change quicker based on recent performance. Value should be between 0 and 1.
+
 #region UI 
 
 const DAY_END_SCREEN_SHOW_TIME_SECONDS: float = 3.0
