c4ae360cd945afe6be9a1adc92540412e8ba276b
Ziana Tudose <tudosesanziana@gmail.com>
2026-07-04 16:38:36 +0200

    Time of Day: integrate shadow assets
    - Add a shadow shader with 2 shadow types: Global and Highlights
    - Refactor how time of day changes are propagated - use a Shader Globals instead of setting param on individual sprites
    - In dialogue view, rely on Render Priority instead of Z position (changing Z position would cause issues with Shadow assets aligning with Color assets)

--- scripts/managers/time_manager.gd
+++ scripts/managers/time_manager.gd
@@ -2,7 +2,6 @@
 class_name TimeManager
 extends Node
 
-var sprites: Array[Sprite3D] = []
 var _current_time_of_day: float = 0.0:
 	set(value):
 		_current_time_of_day = clampf(value, 0.0, 1.0)
@@ -13,8 +12,10 @@ var _current_time_of_day: float = 0.0:
 var _active_tween = null
 
 func _ready() -> void:
-	# Defer this so all child scenes are fully instantiated 
-	call_deferred("_register_sprites")
+	_update_current_time_of_day()
+
+	if OS.is_debug_build():
+		DebugUI.register_debug_target(self)
 
 func set_target_time_of_day(day_progress: float, do_lerp: bool = true) -> void:
 	const ERROR_MARGIN: float = 0.0001
@@ -34,20 +35,9 @@ func set_target_time_of_day(day_progress: float, do_lerp: bool = true) -> void:
 	_active_tween = get_tree().create_tween()
 	_active_tween.tween_property(self, "_current_time_of_day", target_time, Config.TIME_OF_DAY_BASE_LERP_SECONDS).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 
-func _register_sprites() -> void:
-	for node in get_tree().get_nodes_in_group("time_of_day"):
-		if node is Sprite3D:
-			sprites.append(node)
-	
-	_update_current_time_of_day()
-
-	if OS.is_debug_build():
-		DebugUI.register_debug_target(self)
-
 func _update_current_time_of_day() -> void:
-	for sprite in sprites:
-		if sprite.material_override:
-			sprite.material_override.set_shader_parameter("time_of_day", _current_time_of_day)
+	# All relevant sprites have this as a global shader parameter
+	RenderingServer.global_shader_parameter_set("time_of_day", _current_time_of_day)
 
 #region Debug
 
