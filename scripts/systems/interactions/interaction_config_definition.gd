dc3c99b383d6715c5c3fc40d54110b086a98f459
Ziana Tudose <tudosesanziana@gmail.com>
2026-07-31 19:25:47 +0200

    Prologue: Add custom dialogue UI to interaction config
    Each interaction config can now assign a custom scene to use for the dialogue balloon

--- scripts/systems/interactions/interaction_config_definition.gd
+++ scripts/systems/interactions/interaction_config_definition.gd
@@ -6,7 +6,9 @@ class_name InteractionConfigDefinition
 extends Resource
 
 const DEFAULT_SCENE_NAME: String = "DialogueView"
+const DEFAULT_DIALOGUE_BALLOON_SCENE: PackedScene = preload("res://scenes/UI/dialogue_baloon/dialogue_balloon.tscn")
 
 # TODO: Right now, this is used to show/hide existing scene instances. Ideally, this should hold a scene reference and load it / clean up other scenes when needed.
 @export var scene_name: String = DEFAULT_SCENE_NAME
 @export var is_camera_locked: bool = false
+@export var dialogue_balloon_scene: PackedScene = DEFAULT_DIALOGUE_BALLOON_SCENE
